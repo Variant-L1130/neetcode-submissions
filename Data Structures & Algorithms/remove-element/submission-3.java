8ff975bef8df7643258baa031c787f09ccc1a379
Atharv Shukla <atharv.mac.ios@gmail.com>
2026-06-16 23:12:51 +0530

    Add: remove-element - submission-3

---
+++ Data Structures & Algorithms/remove-element/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int m = 0;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]!=val){
+                //int n = nums[m];
+                nums[m] = nums[i];
+                //nums[i] = n;
+
+                m++;
+
+            }
+        }
+        return m;
+        
+        
+    }
+}
